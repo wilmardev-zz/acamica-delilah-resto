b46d10ea78d67a7e7043d8554d6e3142085b1c60
David Henao <david.henao88@gmail.com>
2020-04-21 21:06:50 -0500

    creacion de modelo de clases

---
+++ src/classes/39-intro-back/album.js
@@ -0,0 +1,7 @@
+class Album {
+    constructor(nombreAlbum, anioAlbum, artista) {
+        this.NombreAlbum = nombreAlbum;
+        this.AnioAlbum = anioAlbum;
+        this.Artista = artista;
+    }
+}
---
+++ src/classes/39-intro-back/cancion.js
@@ -0,0 +1,7 @@
+class Cancion {
+    constructor(titulo, anio, artista) {
+        this.Titulo = titulo;
+        this.Anio = anio;
+        this.Artista = artista;
+    }
+}
