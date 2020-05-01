8a219039ba3cc388e18fd97ab93a37244c3d432c
Yulian andres zapata m <hash.key66@gmail.com>
2020-04-30 20:27:12 -0500

    documentation in readme and installing libraries like nodemon and moment

---
+++ index.js
@@ -0,0 +1,7 @@
+var moment = require("moment")
+
+var fecha = new moment("31/12/2019", "DD/MM/YYYY")
+var formateada = fecha.format("MM/DD/YY")
+console.log(formateada)
+
+console.log("hello world acamica --  2020");
