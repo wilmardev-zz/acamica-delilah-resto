8b38f3ea47180772ab6b5a212868399af07c4bc7
Wilmar Duque <wilmarduque71@gmail.com>
2020-05-07 19:38:38 -0500

    add initial code for clas 44-middlewares

---
+++ src/classes/44-middleware/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const server = express();
+const datos = require("../43-express/alumnos.json");
+const port = 3000;
+
+const middlewareGlobal = (req, res, next) => {
+  console.log(
+    `La solicitud del recurso: ${req.path} acaba de pasar por middleware GLOBAL`
+  );
+  next();
+};
+
+const middlewareEspecifico = (req, res, next) => {
+  console.log(
+    `Middleware solo para recurso: ${req.path}`
+  );
+  // Destructuring
+  console.log(req.params);
+  const { user, lastName, name } = req.params;
+  const userTrimmed = user.trim();
+  if (userTrimmed !== "acamica") {
+    console.log("Usuario inválido");
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+  next();
+};
+
+server.use(middlewareGlobal); // Usamos nuestro primer middleware
+// server.use(segundoMiddleware); // Usamos nuestro segundo middleware
+
+server.get("/", (req, res) => {
+  res.json("api acamica");
+});
+
+//Muestra todos los estudiantes
+server.get("/acamica/alumnos/:user", middlewareEspecifico, (req, res) => {
+  res.json(datos);
+});
+
+server.listen(port, () => {
+  console.log("Primer Servidor iniciado en el puerto: " + port);
+});
