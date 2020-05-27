ad211cc63f3e09fb339b988ba36abc08caf9bf3b
Wilmar Duque <wilmarduque71@gmail.com>
2020-05-26 20:42:35 -0500

    update person sql

--- src/classes/49-mysql/persons.sql
+++ src/classes/49-mysql/persons.sql
@@ -1,17 +1,40 @@
--- Create table
-CREATE TABLE person (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  firstName VARCHAR (60),
-  lastame VARCHAR (60) NOT NULL,
-  document INT UNSIGNED NOT NULL
-);
--- Add column
-ALTER TABLE person
-ADD
-  born_date date NOT NULL;
--- Drop column
-ALTER TABLE person DROP fecha_nac;
--- Modify column
-ALTER TABLE person
-MODIFY
-  COLUMN document varchar(10) NOT NULL;
+SELECT *
+FROM persona;
+
+INSERT INTO persona (nombre, apellido, dni, pais, edad, direccion)
+VALUES ('santi', 'zapata', 1332, 'argentina', 20, '');
+
+SELECT nombre, apellido
+FROM persona;
+
+SELECT nombre, apellido
+FROM persona
+LIMIT 5;
+
+SELECT count(*) AS Cantidad
+FROM persona;
+
+-- Modificar Tabla Persona
+ALTER TABLE persona
+ADD edad INT NOT NULL;
+
+ALTER TABLE persona
+ADD direccion VARCHAR(60) NOT NULL;
+
+-- Eliminar Columna tabla persona
+ALTER TABLE persona
+DROP edad;
+
+SELECT * 
+FROM persona
+WHERE dni LIKE '%2' AND edad = 0 AND nombre != 'yulian';
+
+
+DELETE 
+FROM persona
+-- WHERE id = 2
+
+
+-- CREATE, INSERT - UPDATE, ALTER, DELETE, 
+-- SELECT, SELECT - WHERE, DELETE - WHERE, 
+-- SELECT - COUNT, ALTER - DROP
