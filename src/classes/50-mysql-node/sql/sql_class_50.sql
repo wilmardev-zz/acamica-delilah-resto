3c7101716660fdffd1ad48a3f5191e1dadc309f0
Wilmar Duque <wilmarduque71@gmail.com>
2020-05-28 20:41:41 -0500

    update sql file class 50

--- src/classes/50-mysql-node/sql/sql_class_50.sql
+++ src/classes/50-mysql-node/sql/sql_class_50.sql
@@ -1,6 +1,6 @@
 -- Tabla Bandas
 CREATE TABLE Banda (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
     integrantes INT NOT NULL,
     fecha_inicio DATE NOT NULL,
@@ -10,33 +10,36 @@ CREATE TABLE Banda (
 
 -- Tabla Album
 CREATE TABLE Album (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
-    id_banda DATE NULL,
+    id_banda INT NOT NULL,
     fecha_publicacion DATE NOT NULL
 );
 
 -- Tabla Canciones
 CREATE TABLE Cancion (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
     duracion INT NOT NULL,
     id_album INT NOT NULL,
-    id_banda DATE NULL,
+    id_banda INT NOT NULL,
     fecha_publicacion DATE NOT NULL
 );
 
 -- Llave foránea desde album hacia banda
-ALTER TABLE Album
-ADD CONSTRAINT FK_ALBUM_BANDA
-FOREIGN KEY (id_banda) REFERENCES Banda(id);
+ALTER TABLE
+    Album
+ADD
+    CONSTRAINT FK_ALBUM_BANDA FOREIGN KEY (id_banda) REFERENCES Banda(id);
 
 -- Llave foránea desde cancion hacia album
-ALTER TABLE Cancion
-ADD CONSTRAINT FK_CANCION_ALBUM
-FOREIGN KEY (id_album) REFERENCES Album(id);
+ALTER TABLE
+    Cancion
+ADD
+    CONSTRAINT FK_CANCION_ALBUM FOREIGN KEY (id_album) REFERENCES Album(id);
 
 -- Llave foránea desde cancion hacia banda
-ALTER TABLE Cancion
-ADD CONSTRAINT FK_CANCION_BANDA
-FOREIGN KEY (id_banda) REFERENCES Banda(id);
+ALTER TABLE
+    Cancion
+ADD
+    CONSTRAINT FK_CANCION_BANDA FOREIGN KEY (id_banda) REFERENCES Banda(id);
